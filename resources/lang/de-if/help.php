8a4760863e83b49fccc36976453704fadeffe635
Justus <justus@justcoding.tech>
2024-12-22 14:12:06 +0100

    Update translations

--- resources/lang/de-if/help.php
+++ resources/lang/de-if/help.php
@@ -15,21 +15,21 @@ return [
 
     'more_info_title' => 'Mehr Info',
 
-    'audit_help' => 'Mit dem Aktivieren dieser Option wird der Asset-Datensatz an diesen neuen Ort angepasst. Wenn diese Option nicht aktiviert ist, wird der Ort nur im Audit-Log angezeigt.<br><br>Falls das Asset ausgecheckt ist, ändert sich der Ort der Person, des Assets oder des Orts, an dem es ausgecheckt ist, nicht.',
+    'audit_help' => 'Mit dem Aktivieren dieser Option wird der Gegenstands-Datensatz an diesen neuen Ort angepasst. Wenn diese Option nicht aktiviert ist, wird der Ort nur im Audit-Log angezeigt.<br><br>Falls der Gegenstand ausgegeben ist, ändert sich der Ort der Person, des Gegenstands oder des Orts, an dem es ausgecheckt ist, nicht.',
 
-    'assets' => 'Assets sind Elemente, die mit Seriennummer oder einem Asset-Tag versehen sind. Sie sind meist höhere Werte, bei denen die Identifizierung eines bestimmten Gegenstands von Bedeutung ist.',
+    'assets' => 'Gegenstände sind Elemente, die mit Seriennummer oder einem Gegenstands-Tag versehen sind. Sie sind meist höhere Werte, bei denen die Identifizierung eines bestimmten Gegenstands von Bedeutung ist.',
 
-    'categories' => 'Kategorien helfen dir beim Organisieren von Assets. Beispielkategorien sind &quot;PCs&quot;, &quot;Laptops&quot;, &quot;Mobiltelefone&quot;, &quot;Tablets&quot; usw., jedoch kannst du Kategorien nutzen, wie du es für sinnvoll erachtest.',
+    'categories' => 'Kategorien helfen dir beim Organisieren von Gegenständen. Beispielkategorien sind &quot;PCs&quot;, &quot;Laptops&quot;, &quot;Mobiltelefone&quot;, &quot;Tablets&quot; usw., jedoch kannst du Kategorien nutzen, wie du es für sinnvoll erachtest.',
 
     'accessories' => 'Ein Zubehör ist alles, was du an einen Benutzer ausgeben kannst, jedoch keine Seriennummer besitzt (oder es keinen Sinn macht diese zu verwalten). Zum Beispiel: Mäuse und Tastaturen.',
 
-    'companies'     => 'Unternehmen können als einfaches Identifikationsfeld oder zur Begrenzung der Sichtbarkeit von Assets, Benutzer usw. verwendet werden, wenn die volle Unterstützung für Unternehmen in Ihren Admin-Einstellungen aktiviert ist.',
+    'companies'     => 'Unternehmen können als einfaches Identifikationsfeld oder zur Begrenzung der Sichtbarkeit von Gegenständen, Benutzern usw. verwendet werden, wenn die volle Unterstützung für Unternehmen in Ihren Admin-Einstellungen aktiviert ist.',
 
-    'components'    => 'Komponenten sind Teile eines Assets, zum Beispiel eine Festplatte, Arbeitsspeicher, etc.',
+    'components'    => 'Komponenten sind Teile eines Gegenstands, zum Beispiel eine Festplatte, Arbeitsspeicher, etc.',
 
     'consumables'   => 'Verbrauchsmaterialien sind alle gekauften Dinge, die mit der Zeit aufgebraucht werden. Zum Beispiel Druckerpatronen oder Kopierpapier.',
 
-    'depreciations' => 'Du kannst Asset-Abschreibungen einrichten, um Assets linear abzuschreiben.',
+    'depreciations' => 'Du kannst Gegenstand-Abschreibungen einrichten, um Gegenstände linear abzuschreiben.',
     
     'empty_file'    => 'Der Importer hat festgestellt, dass diese Datei leer ist.'
 ];
