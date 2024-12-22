8521294e1e58d3ddaa32e572926b752c508524b7
Justus <justus@justcoding.tech>
2024-12-22 16:26:39 +0100

    Improve cursor experience on Bulk Checkout site

--- resources/views/partials/forms/edit/asset-select.blade.php
+++ resources/views/partials/forms/edit/asset-select.blade.php
@@ -3,7 +3,7 @@
      class="form-group{{ $errors->has($fieldname) ? ' has-error' : '' }}"{!!  (isset($style)) ? ' style="'.e($style).'"' : ''  !!}>
     {{ Form::label($fieldname, $translated_name, array('class' => 'col-md-3 control-label')) }}
     <div class="col-md-7">
-        <select class="js-data-ajax select2" data-endpoint="hardware" data-placeholder="{{ trans('general.select_asset') }}" aria-label="{{ $fieldname }}" name="{{ $fieldname }}" style="width: 100%" id="{{ (isset($select_id)) ? $select_id : 'assigned_asset_select' }}"{{ (isset($multiple)) ? ' multiple' : '' }}{!! (!empty($asset_status_type)) ? ' data-asset-status-type="' . $asset_status_type . '"' : '' !!}{{  ((isset($required) && ($required =='true'))) ?  ' required' : '' }}>
+        <select class="js-data-ajax select2" id="asset_selector" data-endpoint="hardware" data-placeholder="{{ trans('general.select_asset') }}" aria-label="{{ $fieldname }}" name="{{ $fieldname }}" style="width: 100%" id="{{ (isset($select_id)) ? $select_id : 'assigned_asset_select' }}"{{ (isset($multiple)) ? ' multiple' : '' }}{!! (!empty($asset_status_type)) ? ' data-asset-status-type="' . $asset_status_type . '"' : '' !!}{{  ((isset($required) && ($required =='true'))) ?  ' required' : '' }}>
 
             @if ((!isset($unselect)) && ($asset_id = old($fieldname, (isset($asset) ? $asset->id  : (isset($item) ? $item->{$fieldname} : '')))))
                 <option value="{{ $asset_id }}" selected="selected" role="option" aria-selected="true"  role="option">
@@ -16,7 +16,7 @@
                     @if(isset($asset_ids))
                         @foreach($asset_ids as $asset_id)
                             <option value="{{ $asset_id }}" selected="selected" role="option" aria-selected="true"
-                                    role="option" onclick="$('#assets_to_checkout_div').click();">
+                                    role="option">
                                 {{ (\App\Models\Asset::find($asset_id)) ? \App\Models\Asset::find($asset_id)->present()->fullName : '' }}
                             </option>
                         @endforeach
@@ -27,4 +27,9 @@
     </div>
     {!! $errors->first($fieldname, '<div class="col-md-8 col-md-offset-3"><span class="alert-msg" aria-hidden="true"><i class="fas fa-times" aria-hidden="true"></i> :message</span></div>') !!}
 
+    <script>
+        $('#asset_selector').on('select2:select', (e) => {
+            $('#assets_to_checkout_div').click();
+        })
+    </script>
 </div>
