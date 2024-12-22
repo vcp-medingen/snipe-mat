1e3dc6c68d513b8245b95adfbac13b63f690291a
Justus <justus@justcoding.tech>
2024-12-22 16:19:36 +0100

    Improve cursor experience on Bulk Checkout site

--- resources/views/hardware/bulk-checkout.blade.php
+++ resources/views/hardware/bulk-checkout.blade.php
@@ -120,13 +120,6 @@
         //if there's already a user selected, make sure their checked-out assets show up
         // (if there isn't one, it won't do anything)
         $('#assigned_user').change();
-
-        $('#assigned_assets_select').on('keypress', (e) => {
-            if (e.which === 13) {
-                $('#assets_to_checkout_div').click();
-                $(this).focus();
-            }
-        })
     });
 </script>
 
