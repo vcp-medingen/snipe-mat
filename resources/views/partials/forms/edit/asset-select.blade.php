b53e796cb5edfa59ea79b90b871cb4db7de29fee
Justus <justus@justcoding.tech>
2024-12-22 16:28:39 +0100

    Improve cursor experience on Bulk Checkout site

--- resources/views/partials/forms/edit/asset-select.blade.php
+++ resources/views/partials/forms/edit/asset-select.blade.php
@@ -26,10 +26,12 @@
         </select>
     </div>
     {!! $errors->first($fieldname, '<div class="col-md-8 col-md-offset-3"><span class="alert-msg" aria-hidden="true"><i class="fas fa-times" aria-hidden="true"></i> :message</span></div>') !!}
-
-    <script>
-        $('#asset_selector').on('select2:select', (e) => {
-            $('#assets_to_checkout_div').click();
-        })
-    </script>
 </div>
+
+@section('moar_scripts')
+<script>
+    $('#asset_selector').on('select2:select', (e) => {
+        $('#assets_to_checkout_div').click();
+    })
+</script>
+@stop
