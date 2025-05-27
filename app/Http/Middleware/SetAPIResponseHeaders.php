1822027a8f15fe106f7923cbc910c9b61b18c345
snipe <snipe@snipe.net>
2025-05-27 14:04:24 +0100

    Extend the built-in ThrottleRequests middleware from Laravel
    Signed-off-by: snipe <snipe@snipe.net>

--- app/Http/Middleware/SetAPIResponseHeaders.php
+++ app/Http/Middleware/SetAPIResponseHeaders.php
@@ -4,9 +4,42 @@ namespace App\Http\Middleware;
 
 use Closure;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Cache;
+use Illuminate\Support\Facades\RateLimiter;
+use Illuminate\Routing\Middleware\ThrottleRequests;
+use Symfony\Component\HttpFoundation\Response;
 
-class SetAPIResponseHeaders
+class SetAPIResponseHeaders extends ThrottleRequests
 {
+
+    protected function getHeaders($maxAttempts,  $remainingAttempts, $retryAfter = null, ?Response $response = null)
+    {
+        if ($response &&
+            ! is_null($response->headers->get('X-RateLimit-Remaining')) &&
+            (int) $response->headers->get('X-RateLimit-Remaining') <= (int) $remainingAttempts) {
+            \Log::error('Not adding headers');
+            $headers = [];
+            $headers['Retry-After'] = $retryAfter; // this is the only line we changed
+            $headers['X-RateLimit-Reset'] = $this->availableAt($retryAfter); // this is the only line we changed
+            return $headers;
+        }
+
+        $headers = [
+            'X-RateLimit-Limit' => $maxAttempts,
+            'X-RateLimit-Remaining' => $remainingAttempts,
+        ];
+
+        if (! is_null($retryAfter)) {
+            \Log::error('Retry after is set');
+            $headers['Retry-After'] = $retryAfter;
+            $headers['X-RateLimit-Reset'] = $this->availableAt($retryAfter);
+        }
+
+        return $headers;
+    }
+
+
+
     /**
      * Handle an incoming request.
      *
@@ -14,12 +47,28 @@ class SetAPIResponseHeaders
      * @param  \Closure  $next
      * @return mixed
      */
-    public function handle(Request $request, Closure $next)
+    protected function handleRequest($request, Closure $next, array $limits)
     {
+        foreach ($limits as $limit) {
+            if ($this->limiter->tooManyAttempts($limit->key, $limit->maxAttempts)) {
+                throw $this->buildException($request, $limit->key, $limit->maxAttempts, $limit->responseCallback);
+            }
+
+            $this->limiter->hit($limit->key, $limit->decaySeconds);
+        }
+
         $response = $next($request);
-        // $response->headers->set('Retry-After', ($request->header('Retry-After') ?? 60));
-       // $response->headers->set('X-RateLimit-Reset', ($request->header('Retry-After') ?? 60) + time());
-        // $response->headers->set('X-Ratelimit-Used', ($request->header('X-Ratelimit-Limit') - $request->header('X-RateLimit-Remaining')));
+
+        foreach ($limits as $limit) {
+            $response = $this->addHeaders(
+                $response,
+                $limit->maxAttempts,
+                $this->calculateRemainingAttempts($limit->key, $limit->maxAttempts),
+                $this->getTimeUntilNextRetry($limit->key) // this is the only line we changed
+            );
+        }
+
         return $response;
     }
+
 }
