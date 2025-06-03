9eaabf95a00e6b28a2566fb2de298f9e9f35cc87
snipe <snipe@snipe.net>
2025-06-03 11:02:41 +0100

    Make version number match tagged version :(
    Signed-off-by: snipe <snipe@snipe.net>

--- config/version.php
+++ config/version.php
@@ -1,10 +1,10 @@
 <?php
 return array (
-  'app_version' => 'v8.1.5',
-  'full_app_version' => 'v8.1.5 - build 18388-g64aeaeeee',
-  'build_version' => '18388',
+  'app_version' => 'v8.1.15',
+  'full_app_version' => 'v8.1.15 - build 18405-ge4314cf42',
+  'build_version' => '18405',
   'prerelease_version' => '',
-  'hash_version' => 'g64aeaeeee',
-  'full_hash' => 'v8.1.5-141-g64aeaeeee',
+  'hash_version' => 'ge4314cf42',
+  'full_hash' => 'v8.1.15-13-ge4314cf42',
   'branch' => 'develop',
 );
