cd53fc631833856ddf156824bbde3083c0285b21
Marcus Moore <contact@marcusmoore.io>
2025-06-11 16:56:40 -0700

    Scaffold email contents

--- resources/views/mail/markdown/checkout-acceptance-response.blade.php
+++ resources/views/mail/markdown/checkout-acceptance-response.blade.php
@@ -1,6 +1,22 @@
 @component('mail::message')
 # {{ trans('mail.hello') }} {{ $recipient->present()->fullName() }},
 
+{fullName} {accepted|declined} the following checkout:
+
+@component('mail::table')
+|        |          |
+| ------------- | ------------- |
+@if ((isset($item->name)) && ($item->name!=''))
+    | **{{ trans('mail.name') }}** | {{ $item->name }} |
+@endif
+@if (isset($item->asset_tag))
+    | **{{ trans('mail.asset_tag') }}** | {{ $item->asset_tag }} |
+@endif
+@if (isset($note) && $note != '')
+    | **{{ trans('mail.notes') }}** | {{ $note }} |
+@endif
+@endcomponent
+
 {{ trans('mail.best_regards') }}
 
 {{ $snipeSettings->site_name }}
