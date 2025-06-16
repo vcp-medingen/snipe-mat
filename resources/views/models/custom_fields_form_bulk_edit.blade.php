7b889d22d262b13a4f8ce24c3ee14d1e014eea51
snipe <snipe@snipe.net>
2025-06-16 16:14:01 +0100

    Fixed HTML
    Signed-off-by: snipe <snipe@snipe.net>

--- resources/views/models/custom_fields_form_bulk_edit.blade.php
+++ resources/views/models/custom_fields_form_bulk_edit.blade.php
@@ -2,6 +2,14 @@
 //set array up before loop so it doesn't get wiped at every iteration
     $fields = [];
 @endphp
+
+@if (isset($models) && count($models) > 0)
+    <fieldset name="custom-fields" class="bottom-padded">
+        <legend class="highlight">
+            {{ trans('admin/custom_fields/general.custom_fields') }}
+        </legend>
+@endif
+
 @foreach($models as $model)
 @if (($model) && ($model->fieldset))
     @foreach($model->fieldset->fields AS $field)
@@ -30,12 +38,6 @@
                       :selected="old($field->db_column_name(),(isset($item) ? Helper::gracefulDecrypt($field, $item->{$field->db_column_name()}) : $field->defaultValue($model->id)))"
                       class="format form-control"
                   />
-                  <div class="col-md-5">
-                      <label class="form-control">
-                          <input type="checkbox" name="null_asset_eol_date" value="1">
-                          {{ trans_choice('general.set_to_null', count($assets),['selection_count' => count($assets)]) }}
-                      </label>
-                  </div>
 
               @elseif ($field->element=='textarea')
                 @if($field->is_unique)
@@ -98,13 +100,19 @@
 
           @endif
 
+          <p class="help-block">
+              <x-icon type="warning" class="text-info" /> {{ trans('admin/hardware/form.bulk_update_model_prefix') }}:
+              @foreach ($field->assetModels()->pluck('name')->intersect($modelNames) as $modelName)
+                  <span class="label label-default">
+                {{ $modelName }}
+            </span>&nbsp;
+              @endforeach
+          </p>
+
         @if ($field->help_text!='')
             <p class="help-block">{{ $field->help_text }}</p>
         @endif
 
-        <p>{{ trans('admin/hardware/form.bulk_update_model_prefix') }}: 
-                    {{$field->assetModels()->pluck('name')->intersect($modelNames)->implode(', ')}} 
-            </p>     
 
               
               
@@ -124,14 +132,16 @@
         </div>
         @endif
 
-        <div class="col-md-5">
+        <div class="col-md-8 col-md-offset-3" style="padding-bottom: 10px;">
             <label class="form-control">
                 <input type="checkbox" name="{{ 'null'.$field->db_column_name() }}" value="1">
                 {{ trans_choice('general.set_to_null', count($assets),['selection_count' => count($assets)]) }}
             </label>
         </div>
     </div>
-
     @endforeach
 @endif
  @endforeach
+@if (isset($models) && count($models) > 0)
+    </fieldset>
+@endif
