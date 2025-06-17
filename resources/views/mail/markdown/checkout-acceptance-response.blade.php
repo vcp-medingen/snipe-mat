67b32ca14d76d65487c595e41c9ef72ffd45e09a
Marcus Moore <contact@marcusmoore.io>
2025-06-17 14:22:54 -0700

    Mail content improvements

--- resources/views/mail/markdown/checkout-acceptance-response.blade.php
+++ resources/views/mail/markdown/checkout-acceptance-response.blade.php
@@ -3,7 +3,7 @@
 
 {{ $introduction }}:
 
-@if (($snipeSettings->show_images_in_email =='1') && $item->getImageUrl())
+@if (($snipeSettings->show_images_in_email =='1') && (method_exists($item, 'getImageUrl') && $item->getImageUrl()))
 <center><img src="{{ $item->getImageUrl() }}" alt="Asset" style="max-width: 570px;"></center>
 @endif
 
@@ -11,9 +11,7 @@
 |        |          |
 | ------------- | ------------- |
 | **{{ trans('mail.user') }}** | {{ $assignedTo->present()->fullName() }} |
-@if ((isset($item->name)) && ($item->name!=''))
-| **{{ trans('mail.name') }}** | {{ $item->name }} |
-@endif
+| **{{ trans('mail.name') }}** | {{ $item->present()->name() }} |
 @if (isset($item->asset_tag))
 | **{{ trans('mail.asset_tag') }}** | {{ $item->asset_tag }} |
 @endif
