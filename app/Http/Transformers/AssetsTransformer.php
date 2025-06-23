8894bb91cce3c7bae450706e0db90878b58d498d
Godfrey M <godmartinz@gmail.com>
2025-06-23 12:37:29 -0700

    fix method choice in asset transformer for depreciations

--- app/Http/Transformers/AssetsTransformer.php
+++ app/Http/Transformers/AssetsTransformer.php
@@ -102,7 +102,7 @@ class AssetsTransformer
             'checkout_counter' => (int) $asset->checkout_counter,
             'requests_counter' => (int) $asset->requests_counter,
             'user_can_checkout' => (bool) $asset->availableForCheckout(),
-            'book_value' => Helper::formatCurrencyOutput($asset->getLinearDepreciatedValue()),
+            'book_value' => Helper::formatCurrencyOutput($asset->getDepreciatedValue()),
         ];
 
 
