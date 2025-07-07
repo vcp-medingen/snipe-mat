5f4e1835bc936880b341e213fa780dd4316401c7
snipe <snipe@snipe.net>
2025-07-07 12:36:16 +0100

    Removed unused method
    Signed-off-by: snipe <snipe@snipe.net>

--- app/Policies/CompanyPolicy.php
+++ app/Policies/CompanyPolicy.php
@@ -10,12 +10,5 @@ class CompanyPolicy extends SnipePermissionsPolicy
     {
         return 'companies';
     }
-
-    public function canEditThisCompany($company_id = null) {
-        if ((Setting::getSettings()->scope_locations_fmcs) && ($this->company_id == $company_id)){
-            return true;
-        }
-
-        return false;
-    }
+    
 }
