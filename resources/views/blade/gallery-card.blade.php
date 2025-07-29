c3d1987fac65827e8cba411274b05cfea57643f5
snipe <snipe@snipe.net>
2025-07-29 21:44:06 +0100

    Switch to panel
    Signed-off-by: snipe <snipe@snipe.net>

--- resources/views/blade/gallery-card.blade.php
+++ resources/views/blade/gallery-card.blade.php
@@ -1,36 +1,37 @@
 <!-- HTML for the file gallery template -->
 <template id="fileGalleryTemplate">
-    <div class="col-md-4 col-lg-3 col-xl-2" style="height: 400px;">
-            <div class="box box-%PANEL_CLASS%">
-                <div class="box-header with-border">
-                    <h5>
-                        <i class="%ICON%" /></i>
-                        %FILENAME%
-                    </h5>
-                </div>
 
-                <div class="box-body">
-                    <div class="row">
-                        <div class="col-md-12" style="height: 200px; overflow: scroll !important;">
-                            %FILE_EMBED%
-                            <br><br>
-                            <p>
-                            %NOTE%
-                            <br>
-                            %CREATED_AT% - %CREATED_BY%
-                            </p>
-                        </div>
-                    </div>
+    <div class="col-md-4 col-lg-3 col-xl-2" style="height: 400px;">
+
+        <div class="panel panel-%PANEL_CLASS%">
+            <div class="panel-heading">
+                <h3 class="panel-title" style="white-space: nowrap; overflow: hidden; text-overflow: ellipsis;">
+                    <i class="%ICON%" /></i>
+                    %ID% - %FILENAME%
+                </h3>
+            </div>
+            <div class="panel-body">
+                <div class="col-md-12" style="height: 200px; overflow: scroll !important;">
+                    %FILE_EMBED%
+                    <br><br>
+                    <p>
+                        %NOTE%
+                        <br>
+                        %CREATED_AT% - %CREATED_BY%
+                    </p>
                 </div>
-                <div class="box-footer">
-                    <div class="text-left col-md-6">
+            </div>
+            <div class="panel-footer">
+                <div class="row">
+                    <div class="text-left col-md-1">
                         %DELETE_BUTTON%
                     </div>
-                    <div class="text-right col-md-6" style="white-space: nowrap">
+                    <div class="text-right col-md-11" style="white-space: nowrap">
                         %DOWNLOAD_BUTTON% %NEW_WINDOW_BUTTON%
                     </div>
                 </div>
-        </div><!-- /.box -->
+            </div><!-- /.panel-footer -->
+        </div> <!-- /.panel panel-%PANEL_CLASS% -->
     </div><!-- /.col-md-4 col-lg-3 col-xl-1 -->
 </template>
 <!-- ./ HTML for the file gallery template -->
