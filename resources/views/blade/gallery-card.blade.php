4992c778188564ddb72c99a83d828d8c9efe8a04
snipe <snipe@snipe.net>
2025-07-29 05:41:43 +0100

    Updated template
    Signed-off-by: snipe <snipe@snipe.net>

--- resources/views/blade/gallery-card.blade.php
+++ resources/views/blade/gallery-card.blade.php
@@ -1,26 +1,32 @@
 <!-- HTML for the file gallery template -->
 <template id="fileGalleryTemplate">
-    <div class="col-md-3 col-lg-2 col-xl-1" style="height: 300px;">
+    <div class="col-md-4 col-lg-3 col-xl-2" style="height: 400px;">
             <div class="box box-%PANEL_CLASS%">
                 <div class="box-header with-border">
-                    <h3 class="box-title">
+                    <h5>
                         <i class="%ICON%" /></i>
-                        %FILENAME% (%ID%)
-                    </h3>
+                        %FILENAME%
+                    </h5>
                 </div>
 
                 <div class="box-body">
-                    <div class="col-md-12">
-                        %INLINE_IMAGE%
-                        <br>
-                        %NOTE%
+                    <div class="row">
+                        <div class="col-md-12" style="height: 200px; overflow: scroll !important;">
+                            %FILE_EMBED%
+                            <br><br>
+                            <p>
+                            %NOTE%
+                            <br>
+                            %CREATED_AT% - %CREATED_BY%
+                            </p>
+                        </div>
                     </div>
                 </div>
                 <div class="box-footer">
                     <div class="text-left col-md-6">
                         %DELETE_BUTTON%
                     </div>
-                    <div class="text-right col-md-6">
+                    <div class="text-right col-md-6" style="white-space: nowrap">
                         %DOWNLOAD_BUTTON% %NEW_WINDOW_BUTTON%
                     </div>
                 </div>
