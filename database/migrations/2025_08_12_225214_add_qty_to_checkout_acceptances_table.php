03d3fb6a5f2d57b81698c4f0efc5f93e49e1d157
Marcus Moore <contact@marcusmoore.io>
2025-08-13 12:09:56 -0700

    Add qty to checkout_acceptances table

---
+++ database/migrations/2025_08_12_225214_add_qty_to_checkout_acceptances_table.php
@@ -0,0 +1,27 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration {
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::table('checkout_acceptances', function (Blueprint $table) {
+            $table->unsignedInteger('qty')->nullable()->after('assigned_to_id')->default(null);
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::table('checkout_acceptances', function (Blueprint $table) {
+            $table->dropColumn('qty');
+        });
+    }
+};
