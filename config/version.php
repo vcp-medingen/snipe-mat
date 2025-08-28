9c0202e5ce5aad068e5deb5727c402bfb5e2b194
snipe <snipe@snipe.net>
2025-08-28 18:33:46 +0100

    Bumped to 8.3.0
    Signed-off-by: snipe <snipe@snipe.net>

--- config/version.php
+++ config/version.php
@@ -1,10 +1,10 @@
 <?php
 return array (
-  'app_version' => 'v8.2.2',
-  'full_app_version' => 'v8.2.2 - build 19559-g0ba8f5cc5',
+  'app_version' => 'v8.3.0',
+  'full_app_version' => 'v8.3.0 - build 19559-g0ba8f5cc5',
   'build_version' => '19559',
   'prerelease_version' => '',
   'hash_version' => 'g0ba8f5cc5',
-  'full_hash' => 'v8.2.2-489-g0ba8f5cc5',
+  'full_hash' => 'v8.3.0-489-g0ba8f5cc5',
   'branch' => 'develop',
 );
