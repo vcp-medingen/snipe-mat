b9cfc03b4fed260a06ead6495c16d981803559d8
snipe <snipe@snipe.net>
2025-09-02 11:30:34 +0100

    display name fix
    Signed-off-by: snipe <snipe@snipe.net>

--- app/Http/Transformers/ProfileTransformer.php
+++ app/Http/Transformers/ProfileTransformer.php
@@ -25,7 +25,7 @@ class ProfileTransformer
             'id' => (int) $file->id,
             'icon' => Helper::filetype_icon($file->filename),
             'item' => ($file->item) ? [
-                'name' => ($file->itemType()=='user') ? e($file->item->display_name) : e($file->item->getDisplayNameAttribute()),
+                'name' => $file->item->display_name ? e($file->item->display_name) : null,
                 'type' => e($file->itemType()),
             ] : null,
             'filename' => e($file->filename),
