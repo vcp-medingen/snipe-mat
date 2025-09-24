c46227ee945ce2b2c15a329cbcf1c7fdbc7a4c5d
Brady Wetherington <bwetherington@grokability.com>
2025-09-24 14:28:49 +0100

    Fix to the checkout-selector issue

--- resources/views/partials/forms/checkout-selector.blade.php
+++ resources/views/partials/forms/checkout-selector.blade.php
@@ -4,22 +4,25 @@
 
         <div class="btn-group" data-toggle="buttons">
             @if ((isset($user_select)) && ($user_select!='false'))
-            <label class="btn btn-default{{ session('checkout_to_type') == 'user' ? ' active' : '' }}">
-                <input name="checkout_to_type" value="user" aria-label="checkout_to_type" type="radio" checked="checked">
+                <label class="btn btn-default{{ (session('checkout_to_type') ?? 'user') == 'user' ? ' active' : '' }}">
+                    <input name="checkout_to_type" value="user" aria-label="checkout_to_type"
+                           type="radio" {{ (session('checkout_to_type') ?? 'user') == 'user' ? 'checked' : '' }}>
                 <x-icon type="user" />
                 {{ trans('general.user') }}
             </label>
             @endif
             @if ((isset($asset_select)) && ($asset_select!='false'))
-            <label class="btn btn-default{{ session('checkout_to_type') == 'asset' ? ' active' : '' }}">
-                <input name="checkout_to_type" value="asset" aria-label="checkout_to_type" type="radio">
+                <label class="btn btn-default{{ session('checkout_to_type') == 'asset' ? ' active' : '' }}">
+                    <input name="checkout_to_type" value="asset" aria-label="checkout_to_type"
+                           type="radio" {{ session('checkout_to_type') == 'asset' ? 'checked': '' }}>
                 <i class="fas fa-barcode" aria-hidden="true"></i>
                 {{ trans('general.asset') }}
             </label>
             @endif
             @if ((isset($location_select)) && ($location_select!='false'))
-            <label class="btn btn-default{{ session('checkout_to_type') == 'location' ? ' active' : '' }}">
-                <input name="checkout_to_type" value="location" aria-label="checkout_to_type" class="active" type="radio">
+                <label class="btn btn-default{{ session('checkout_to_type') == 'location' ? ' active' : '' }}">
+                    <input name="checkout_to_type" value="location" aria-label="checkout_to_type" class="active"
+                           type="radio" {{ session('checkout_to_type') == 'location' ? 'checked' : '' }}>
                 <i class="fas fa-map-marker-alt" aria-hidden="true"></i>
                 {{ trans('general.location') }}
             </label>
